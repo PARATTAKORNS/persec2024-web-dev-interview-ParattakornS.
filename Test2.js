78410e9d9a31d7f5745bc1acc4727205096e6d39
PP.PHIPEA <147033148+PARATTAKORNS@users.noreply.github.com>
2024-07-02 15:37:47 +0700

    Update and rename Test2

--- Test2.js
+++ Test2.js
@@ -1,22 +1,19 @@
 function sortMixedArray(arr) {
     return arr.sort((a, b) => {
-        // แยกตัวเลขกับพยัญชนะ
-        let num1 = parseInt(a.match(/\d+/g));
-        let num2 = parseInt(b.match(/\d+/g));
-        console.log(num1,num2);
-
-        // แยกตัวอักษร
+        // หาอักษร
         let str1 = a.match(/[a-zA-Z]+/g).join('');
         let str2 = b.match(/[a-zA-Z]+/g).join('');
         console.log(str1,str2);
-
+        // หาตัวเลข
+        let num1 = parseInt(a.match(/\d+/g));
+        let num2 = parseInt(b.match(/\d+/g));
+        console.log(num1,num2);
         // เทียบอักษร
         if (str1 < str2){
             return -1
         } else {
             return 1
         }
-
         // เทียบตัวเลข
         return num1 - num2;
     });
